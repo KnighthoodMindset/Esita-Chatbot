807085676ee412d5fc63f7b6778d5132123c21de
KnighthoodMindset <sweetyseleena27@gmail.com>
2026-01-30 05:42:40 +0530

    Updated requirements with all packages

--- Backend/main.py
+++ Backend/main.py
@@ -4,6 +4,9 @@ from dotenv import load_dotenv
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
+import requests
+import something_else
+
 
 load_dotenv()
 
